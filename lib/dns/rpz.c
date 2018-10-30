2087adecc5677ffcd237956fe29cbd04cbc0cb17
Witold Kręcicki <wpk@isc.org>
2018-10-30 07:09:58 +0000

    Destroy task first when destroying rpzs.
    When freeing rpzs structures we need to kill the updater task first.
    Otherwise we might race with the updater and there might be a crash
    on shutdown.
    
    (cherry picked from commit 541872bf3b1d7d5d30a2aaeb0d19f36829711161)

--- lib/dns/rpz.c
+++ lib/dns/rpz.c
@@ -2171,6 +2171,8 @@ dns_rpz_detach_rpzs(dns_rpz_zones_t **rpzsp) {
 		return;
 	}
 
+	isc_task_destroy(&rpzs->updater);
+
 	/*
 	 * Forget the last of view's rpz machinery after the last reference.
 	 */
@@ -2197,7 +2199,6 @@ dns_rpz_detach_rpzs(dns_rpz_zones_t **rpzsp) {
 	DESTROYLOCK(&rpzs->maint_lock);
 	isc_rwlock_destroy(&rpzs->search_lock);
 	isc_refcount_destroy(&rpzs->refs);
-	isc_task_destroy(&rpzs->updater);
 	isc_mem_putanddetach(&rpzs->mctx, rpzs, sizeof(*rpzs));
 }
 
