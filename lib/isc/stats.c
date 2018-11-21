2015de2c613882f66d558971da78817dfa4b771b
Witold Kręcicki <wpk@isc.org>
2018-11-21 21:27:12 +0000

    16 buckets; non-uniform random

--- lib/isc/stats.c
+++ lib/isc/stats.c
@@ -28,7 +28,7 @@
 #include <isc/stats.h>
 #include <isc/util.h>
 
-#define CBUCKETS 8
+#define CBUCKETS 16
 #define ISC_STATS_MAGIC			ISC_MAGIC('S', 't', 'a', 't')
 #define ISC_STATS_VALID(x)		ISC_MAGIC_VALID(x, ISC_STATS_MAGIC)
 
@@ -146,7 +146,7 @@ isc_stats_increment(isc_stats_t *stats, isc_statscounter_t counter) {
 	REQUIRE(ISC_STATS_VALID(stats));
 	REQUIRE(counter < stats->ncounters);
 
-	atomic_fetch_add_explicit(&stats->counters[counter + isc_random_uniform(CBUCKETS) * stats->ncounters], 1,
+	atomic_fetch_add_explicit(&stats->counters[counter + (isc_random8() % CBUCKETS) * stats->ncounters], 1,
 				  memory_order_relaxed);
 }
 
@@ -155,7 +155,7 @@ isc_stats_decrement(isc_stats_t *stats, isc_statscounter_t counter) {
 	REQUIRE(ISC_STATS_VALID(stats));
 	REQUIRE(counter < stats->ncounters);
 
-	atomic_fetch_sub_explicit(&stats->counters[counter + isc_random_uniform(CBUCKETS) * stats->ncounters], 1,
+	atomic_fetch_sub_explicit(&stats->counters[counter + (isc_random8() % CBUCKETS) * stats->ncounters], 1,
 				  memory_order_relaxed);
 }
 
