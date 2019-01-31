9b75eba5d8de25a4b3ddcbaae8ce0a23ab0270fc
Ondřej Surý <ondrej@sury.org>
2019-01-31 11:31:02 +0100

    Add more atomic_<foo>_relaxed convenience functions

--- lib/isc/include/isc/atomic.h
+++ lib/isc/include/isc/atomic.h
@@ -21,7 +21,24 @@
  * We define a few additional macros to make things easier
  */
 
-#define atomic_store_relaxed(o, v) atomic_store_explicit((o), \
-							 (v), \
-							 memory_order_relaxed)
-#define atomic_load_relaxed(o) atomic_load_explicit((o), memory_order_relaxed)
+#define atomic_store_relaxed(obj, desired) \
+	atomic_store_explicit((obj), (desired), memory_order_relaxed)
+
+#define atomic_load_relaxed(obj) \
+	atomic_load_explicit((obj), memory_order_relaxed)
+
+#define atomic_fetch_add_relaxed(obj, arg) \
+	atomic_fetch_add_explicit((obj), (arg), memory_order_relaxed)
+
+#define atomic_fetch_sub_relaxed(obj, arg) \
+	atomic_fetch_sub_explicit((obj), (arg), memory_order_relaxed)
+
+#define atomic_compare_exchange_strong_relaxed(obj, expected, desired) \
+	atomic_compare_exchange_strong_explicit((obj), (expected), (desired), \
+						memory_order_relaxed,	\
+						memory_order_relaxed)
+
+#define atomic_compare_exchange_weak_relaxed(obj, expected, desired)	\
+	atomic_compare_exchange_weak_explicit((obj), (expected), (desired), \
+					      memory_order_relaxed,	\
+					      memory_order_relaxed)
